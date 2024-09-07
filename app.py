6c4154c4214f2c89d693d374113c49dfd71bae5e
Yale Huang <calvino.huang@gmail.com>
2024-09-07 10:49:02 +0000

    Remove defuncted Streamlit multi-page app.

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([
-    st.Page("app/streamlit_sample_generator.py", title="Sample Generator", icon=":material/text_snippet:"),
-    st.Page("app/streamlit_meta_prompt.py", title="Meta Prompt", icon=":material/auto_fix_high:"),
-])
-
-pg.run()
