e7a86737ae30e3ef1dfc5f54d39610a205bbf08e
Yale Huang <calvino.huang@gmail.com>
2024-10-03 11:44:22 +0000

    Dynamic rendering.

--- demo/dynamic_gradio/gradio_dynamic_ui.py
+++ demo/dynamic_gradio/gradio_dynamic_ui.py
@@ -49,7 +49,7 @@ with gr.Blocks() as demo:
             code_input = gr.Code(label="Enter your Python code", language="python")
             auto_update = gr.Checkbox(label="Auto Update", value=True)
             update_button = gr.Button("Update")
-            code_to_run = gr.Code(label="Code to run", language="python")
+            code_to_run = gr.Code(label="Code to run", language="python", interactive=False)
         
         with gr.Column(scale=2):
             output_blocks = gr.Blocks()
@@ -62,6 +62,32 @@ with gr.Blocks() as demo:
     code_input.change(auto_update_code, [code_input, auto_update], [code_to_run, output, error])
     auto_update.change(auto_update_code, [code_input, auto_update], [code_to_run, output, error])
     update_button.click(manual_update_code, [code_input], [code_to_run, output, error])
+
+    @gr.render(inputs=[code_to_run])
+    def execute_and_render(code):
+        if not code:
+            return gr.Markdown("## No input provided")
+        
+        try:
+            local_scope = {}
+            exec(code, globals(), local_scope)
+        except Exception as e:
+            tb = traceback.format_exc()
+            error_message = f"""
+## Error Details
+```
+<traceback>
+{tb}
+</traceback>
+<error>
+{str(e)}
+</error>
+<code>
+{code}
+</code>
+```
+"""
+            gr.Markdown(error_message)
 
 if __name__ == "__main__":
     demo.launch()
